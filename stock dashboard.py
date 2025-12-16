c1c11dc0a093467f9c310cc53701f5cef7b9d2ea
Ansh Kunwar <anshkunwar3009@gmail.com>
2025-12-16 22:11:49 +0530

    Update stock dashboard.py

--- stock dashboard.py
+++ stock dashboard.py
@@ -451,7 +451,6 @@ with tab3:
 
 with tab4:
     st.subheader(f"📰 Latest News for {selected}")
-    st.markdown(we are facing some issues in news section , will be solved soon !)
     news_items = fetch_news(selected_symbol)
 
     if news_items:
@@ -805,6 +804,7 @@ st.sidebar.markdown("<br><center>© 2025 Live Stock Dashboard</center>", unsafe_
     
 # ---- Footer ----
 st.markdown("<p style='text-align:center; color:white;'>© 2025 Live Stock Dashboard | Powered by Yahoo Finance</p>", unsafe_allow_html=True)
+
 
 
 
